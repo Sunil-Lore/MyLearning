6652cd7efe001e83b5fc536db39c63a87dae2f89
Sunil Kumar <sunil.lore@outlook.com>
2023-02-07 15:38:52 +0530

    Verify key values in Python

---
+++ Python/verify_key_in_dictionary.py
@@ -0,0 +1,7 @@
+a={"Ambani":1,"Adani":2,"Tata":3,"Birla":4}
+# As Adani is in above dictionary it will show as True
+print("Result 1:")
+print("Adani" in a)
+# As Dalmia is not in above dictionary it will show as False
+print("Result 2:")
+print("Dalmia" in a)
