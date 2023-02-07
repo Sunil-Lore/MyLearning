0de0ff66cad515d87b8dac70dc29a97dfc49ac93
Sunil Kumar <sunil.lore@outlook.com>
2023-02-07 20:51:37 +0530

    elif statement

---
+++ Python/elif_statement.py
@@ -0,0 +1,9 @@
+# Developer: Sunil
+# Code: elif statement
+age=18
+if age>18:
+    print("Apply for a driving license!")
+elif age==18:
+    print("Apply for a learner's license!")
+else:
+    print("Please wait!")
