69d9191b2e097df2333d2b73ec9e48d01cdafcf8
Sunil Kumar <sunil.lore@outlook.com>
2023-02-07 15:29:36 +0530

    Adding dictionary elements

---
+++ Python/adding_dict_elements.py
@@ -0,0 +1,5 @@
+a={"Ambani":1,"Adani":2,"Tata":3,"Birla":4}
+print("Dictionary values are:\n",a)
+print("Value of Adani is:",a["Adani"])
+a["Dalmia"]=10
+print("New elements in dictionary are:\n",a)
