85b96aae3582cfbd5156df79c98830d41d59b415
Sunil Kumar <sunil.lore@outlook.com>
2023-02-09 22:30:13 +0530

    hide class elements

--- jQuery/hide_class_elements.js
+++ jQuery/hide_class_elements.js
@@ -1,5 +1,5 @@
 /* Developer: Sunil Kumar
-     Code Purpose: Hide a specific element with a unique ID of a page by clicking a button */
+    Code Purpose: Hide a specific element with a unique ID of a page by clicking a button */
 $(function(){
     $("Button").click(function(){
         $(".hidethisclass").hide();
