7385b0ad84fa31b1e115141d9ff5d6c36011fe6f
Sunil Kumar <sunil.lore@outlook.com>
2023-02-10 20:34:38 +0530

    select element with a specific attribute

---
+++ jQuery/select_attribute_element.js
@@ -0,0 +1,7 @@
+/* Developer: Sunil Kumar
+     Code Purpose: select element having a specific attribute */
+$(function(){
+    $("button").click(function(){
+        $("[href]").hide();
+    });
+});
