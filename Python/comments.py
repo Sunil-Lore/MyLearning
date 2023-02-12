c639ab50c174c002467f9d39945b1e19bce31ad0
Sunil Kumar <sunil.lore@outlook.com>
2023-02-12 23:22:26 +0530

    comments in python

---
+++ Python/comments.py
@@ -0,0 +1,11 @@
+# This is first and important comment in Python
+print("Hello World!") # This is an inline comment.
+# Line 1
+# Line 2
+# Line 3
+# this is multiline comment
+'''
+The following line cannot be printed
+as this is a multiline comment also.
+Hello World
+'''
