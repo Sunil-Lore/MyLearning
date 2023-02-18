1c0300bce065a01fcbbaffb5d26efe0d5cdfbe72
Sunil Kumar <sunil.lore@outlook.com>
2023-02-18 17:02:10 +0530

    change item range in list

---
+++ Python/change_list_itemRange.py
@@ -0,0 +1,7 @@
+# Kindly star my GitHub repo if you like
+# https://github.com/Sunil-Lore
+myList=['Carrot','Potato','Raddish']
+myList[1:3]=['Tomato','Onion']
+print(myList)
+# Developer: Sunil Kumar
+# https://www.DeveloperSunil.in
