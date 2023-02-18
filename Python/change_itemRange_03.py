48b557a73ef63e84b4cb1075055d447e767feebf
Sunil Kumar <sunil.lore@outlook.com>
2023-02-18 17:07:20 +0530

    change less item in  a list

---
+++ Python/change_itemRange_03.py
@@ -0,0 +1,7 @@
+# Kindly star my GitHub repo if you like
+# https://github.com/Sunil-Lore
+myList=['Onion','Raddish','Potato']
+myList[1:3]=['Carrot']
+print(myList)
+# Developer: Sunil Kumar
+# https://www.DeveloperSunil.in
