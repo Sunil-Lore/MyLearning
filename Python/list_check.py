66f0b052c8f60f641ac886bf319b71f39f5a000d
Sunil Kumar <sunil.lore@outlook.com>
2023-02-18 16:43:34 +0530

    Check any item in a list

--- Python/list_check.py
+++ Python/list_check.py
@@ -1,4 +1,5 @@
+# Developer: Sunil Kumar
 myList=["Carrot", "Potato", "Capsicum"]
-if "Potato" in myList:
-    print('"Yes", "Potato" is in the list')
+if "Potato" in myList: # Here 'in' is the keyword which checks the item in given list
+    print('"Yes", "Potato" is in the list') # visit https://www.developersunil.in for more
     
