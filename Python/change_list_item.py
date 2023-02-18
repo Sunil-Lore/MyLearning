d8c72d9f1c6f993a2211666355651b9b1b10ff30
Sunil Kumar <sunil.lore@outlook.com>
2023-02-18 16:58:24 +0530

    change item in a list

---
+++ Python/change_list_item.py
@@ -0,0 +1,8 @@
+# Developer: Sunil
+# For more Python programs kindly visit 
+# https://www.DeveloperSunil.in
+myList=["Carrot", "Potato","Raddish"]
+myList[1]="Tomato"
+print(myList)
+# Kindly star my GitHub repo if you like
+# https://github.com/Sunil-Lore
