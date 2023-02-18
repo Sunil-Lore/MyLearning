f65ddde15390ab2b850f9d5cc0e6f90965e486e7
Sunil Kumar <sunil.lore@outlook.com>
2023-02-18 17:05:22 +0530

    change more items in list

---
+++ Python/change_itemRange_02.py
@@ -0,0 +1,7 @@
+# Kindly star my GitHub repo if you like
+# https://github.com/Sunil-Lore
+myList=['Carrot', 'Onion','Potato']
+myList[1:2]=['Radish','Brinjal']
+print(myList)
+# Developer: Sunil Kumar
+# https://www.DeveloperSunil.in
