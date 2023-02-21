4daf046f09abc6073d149e67cfe5e742bbde5eed
Sunil Kumar <sunil.lore@outlook.com>
2023-02-21 10:58:04 +0530

    range of indexes in lists

---
+++ Python/range_of_list_indexes.py
@@ -0,0 +1,6 @@
+# Kindly star my GitHub repo if you like
+# https://github.com/Sunil-Lore
+myList=['cabbage','onion','potato','carrot','raddish']
+print(myList[2:4])
+# Developer: Sunil Kumar
+# https://www.DeveloperSunil.in
