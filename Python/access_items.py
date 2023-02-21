2091599ea0e34a19cad200ada384652b9fa67f9b
Sunil Kumar <sunil.lore@outlook.com>
2023-02-21 10:53:14 +0530

    Access List Items

--- Python/access_items.py
+++ Python/access_items.py
@@ -1,2 +1,6 @@
 myList=['carrot','raddish','potato','onion','cabbage']
+# Developer: Sunil Kumar
+# https://www.DeveloperSunil.in
 print(myList[1])
+# Kindly star my GitHub repo if you like
+# https://github.com/Sunil-Lore
