edac32daeb7fdf7a9e5ee13da1508781eaf820ee
Sunil Kumar <sunil.lore@outlook.com>
2023-02-21 10:55:38 +0530

    negative list item indexing

---
+++ Python/negative_list_indexing.py
@@ -0,0 +1,6 @@
+# Kindly star my GitHub repo if you like
+# https://github.com/Sunil-Lore
+myList=['carrot', 'raddish','cabbage','onion']
+print(myList[-1])
+# Developer: Sunil Kumar
+# https://www.DeveloperSunil.in
