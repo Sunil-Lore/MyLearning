021f9ff9cf221671a95221c836fc8c15a372695e
Sunil Kumar <sunil.lore@outlook.com>
2023-02-23 22:09:12 +0530

    insert item in a list

--- Python/insert_item_list.py
+++ Python/insert_item_list.py
@@ -1,7 +1,7 @@
 # Developer: Sunil Kumar
 # https://www.DeveloperSunil.in
 myList=['Carrot','Potato']
-myList.insert(1,'Raddish') # inserting 'Raddish' as the 1st item
+myList.insert(1,'Raddish') # inserting 'Raddish' as the item (2nd position)
 print(myList)
 # Kindly star my GitHub repo if you like
 # https://github.com/Sunil-Lore
