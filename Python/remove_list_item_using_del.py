cfd367aa46046f5b9e82f12e6b7026e5eeb20772
Sunil Kumar <sunil.lore@outlook.com>
2023-02-24 22:48:40 +0530

    remove list item using del command

---
+++ Python/remove_list_item_using_del.py
@@ -0,0 +1,7 @@
+# Developer: Sunil Kumar
+# https://www.DeveloperSunil.in
+myList=['onion','potato','tomato']
+# Kindly star my GitHub repo if you like
+# https://github.com/Sunil-Lore
+del myList[1]
+print(myList)
