df215aa30bc66b0616407160028adb210830d06e
Sunil Kumar <sunil.lore@outlook.com>
2023-02-24 22:57:12 +0530

    clear a list

---
+++ Python/clear_list.py
@@ -0,0 +1,7 @@
+# Developer: Sunil Kumar
+# https://www.DeveloperSunil.in
+myList=['onion','potato']
+myList.clear()
+# Kindly star my GitHub repo if you like
+# https://github.com/Sunil-Lore
+print(myList)
