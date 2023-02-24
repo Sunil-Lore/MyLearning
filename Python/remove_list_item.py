a6cfc8c2b20c8f91ae75b37b2e8031b4a20f7875
Sunil Kumar <sunil.lore@outlook.com>
2023-02-24 22:32:46 +0530

    remove item from a list

---
+++ Python/remove_list_item.py
@@ -0,0 +1,7 @@
+# Kindly star my GitHub repo if you like
+# https://github.com/Sunil-Lore
+myList=['carrot','raddish','cabbage']
+myList.remove('raddish')
+# Developer: Sunil Kumar
+# https://www.DeveloperSunil.in
+print(myList)
